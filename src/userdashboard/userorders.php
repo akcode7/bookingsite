fcbd4a6b8fd7d4bd271fa21a2cf2f36ee8a0db86
Ankit Sharma <ankitengineer@hotmail.com>
2024-02-01 12:20:51 +0530

    changes in userorders

--- src/userdashboard/userorders.php
+++ src/userdashboard/userorders.php
@@ -641,7 +641,7 @@ $conn->close();
             <!-- Search input -->
             <div class="flex justify-center flex-1 lg:mr-32">
               <div
-                class="relative w-full max-w-xl mr-6 focus-within:text-purple-500"
+                class="relative w-full max-w-xl mr-6 "
               >
                 <div class="absolute inset-y-0 flex items-center pl-2">
                   <svg
