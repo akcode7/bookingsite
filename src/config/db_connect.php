ae778d1309886d3b8290d77da9cb3fd4187371e1
Ankit Sharma <ankitengineer@hotmail.com>
2024-02-16 19:59:22 +0530

    cancel popup

--- src/config/db_connect.php
+++ src/config/db_connect.php
@@ -12,3 +12,16 @@ if(!$conn){
 }
 
 ?>
+<?php 
+
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "bookingsite";
+
+// $conn = mysqli_connect($server, $username, $password, $database);
+// if(!$conn){
+//     echo "error";
+// }
+
+?>
