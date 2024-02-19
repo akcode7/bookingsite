9f1f9c6b9bbb81dac8c566bac1b49f128878ce68
Ankit Sharma <ankitengineer@hotmail.com>
2024-02-20 00:11:20 +0530

    user dashboard active element

--- src/userdashboard/orderhistorydetail.php
+++ src/userdashboard/orderhistorydetail.php
@@ -84,12 +84,9 @@ $conn->close();
           
           <ul>
             <li class="relative px-6 py-3">
-              <span
-                class="absolute inset-y-0 left-0 w-1 theme_color rounded-tr-lg rounded-br-lg"
-                aria-hidden="true"
-              ></span>
+             
               <a
-                class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                class="inline-flex items-center w-full text-sm font-semibold  transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 "
                 href="userprofile.php"
               >
                 <svg
@@ -110,8 +107,12 @@ $conn->close();
               </a>
             </li>
             <li class="relative px-6 py-3">
+            <span
+                class="absolute inset-y-0 left-0 w-1 theme_color rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"
+              ></span>
               <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
                 href="userorders.php"
               >
                 <svg
