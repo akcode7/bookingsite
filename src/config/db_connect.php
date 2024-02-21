1b5635cda958998c58056c738810b0392b33b12b
Ankit Sharma <ankitengineer@hotmail.com>
2024-02-22 02:19:48 +0530

    database connection

--- src/config/db_connect.php
+++ src/config/db_connect.php
@@ -1,28 +1,28 @@
 
 <?php 
 
-// $server = "localhost";
-// $username = "u441927066_root";
-// $password = "?i]vH?8zWsS43";
-// $database = "u441927066_bookingsite";
-
-// $conn = mysqli_connect($server, $username, $password, $database);
-// if(!$conn){
-//     echo "error";
-// }
-
-// ?>
-
-<?php 
-
 $server = "localhost";
-$username = "root";
-$password = "";
-$database = "bookingsite";
+$username = "u441927066_root";
+$password = "?i]vH?8zWsS43";
+$database = "u441927066_bookingsite";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 if(!$conn){
     echo "error";
 }
+
+// ?>
+
+<?php 
+ 
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "bookingsite";
+
+// $conn = mysqli_connect($server, $username, $password, $database);
+// if(!$conn){
+//     echo "error";
+// }
 
 ?>
