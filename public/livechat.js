4104f1dbf3886046542a7dd5a68b4a0c739171e4
Ankit Sharma <ankitengineer@hotmail.com>
2024-02-27 00:14:04 +0530

    twakto

--- public/livechat.js
+++ public/livechat.js
@@ -2,9 +2,8 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/65db812c8d261e1b5f653c3a/1hngn15ji';
+s1.src='https://embed.tawk.to/65d867739131ed19d970a3af/1hnal87d7';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-
